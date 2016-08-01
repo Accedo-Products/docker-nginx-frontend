56a64bc7380ecc2dc110a8b473359e0a3a9c5d48
Jens Skott <jens.skott@gmail.com>
2016-08-01 10:23:09 +0200

    fixed more stuff on nginx dynamic settings

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM nginx
 
 # Copy custom configuration file from the current directory
 COPY nginx.conf.template /etc/nginx/conf.d/nginx.conf.template
+COPY log.conf /etc/nginx/conf.d/log.conf
 
 CMD ["/bin/bash", "-c", "envsubst < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf && nginx -g 'daemon off;'"]
--- runtest.sh
+++ runtest.sh
@@ -2,9 +2,8 @@
 
 docker build -t mardle/nginx-lb .
 sleep 5
-docker run -e "PORT=8080" -e "APP=web" -e "FORWARD=true" --name nginx-lb -p 33000:80 -p 33443:443 --expose 443 --expose 80 -d mardle/nginx-lb
-sleep 10
-docker exec -i -t nginx-lb cat /etc/nginx/nginx.conf
+docker run -e "DOMAIN=dev.viago.io" -e "PORT=8080" -e "APP=localhost" -e "FORWARD=true" --name nginx-lb -p 33000:80 -p 33443:443 --expose 443 --expose 80 -d mardle/nginx-lb
+sleep 5
 docker logs nginx-lb
 
 docker stop nginx-lb
